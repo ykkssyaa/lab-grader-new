f356f536eddcf57c80698a7908daabfab2bd5f42
ykkssyaa <mr.ender.03@mail.ru>
2024-07-06 21:16:37 +0300

    labs of group in course

--- google_docs.py
+++ google_docs.py
@@ -7,9 +7,12 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 
+LABS_SHEETS_RANGE = "C2:AA2"
+
 creds = None
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
 if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
@@ -19,12 +22,43 @@ if not creds or not creds.valid:
 
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
-            "gateway/credentials.json", SCOPES
+            "credentials.json", SCOPES
         )
         creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
     with open("token.json", "w") as token:
         token.write(creds.to_json())
+
+
+def get_course_groups(google_spreadsheet_id: str) -> list[str]:
+
+    if creds is None:
+        print("get_course_groups: Credentials not loaded.")
+        return []
+
+    service = build("sheets", "v4", credentials=creds)
+    spreadsheet = service.spreadsheets().get(spreadsheetId=google_spreadsheet_id).execute()
+
+    sheets = [sheet['properties']['title'] for sheet in spreadsheet.get('sheets', [])]
+
+    return sheets
+
+
+def get_course_group_labs(google_spreadsheet_id: str, group: str) -> list[str]:
+
+    if creds is None:
+        print("get_course_groups: Credentials not loaded.")
+        return []
+
+    service = build("sheets", "v4", credentials=creds)
+    spreadsheet_range = f"{group}!{LABS_SHEETS_RANGE}"
+
+    spreadsheet = service.spreadsheets().values().get(
+        spreadsheetId=google_spreadsheet_id,
+        range=spreadsheet_range
+    ).execute()
+
+    return spreadsheet.get("values", [])[0]
 
 
 def main():
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import yaml
 from fastapi import FastAPI, Path, HTTPException
-# from gateway import google_docs, github_api
+import google_docs
 import os
 
 app = FastAPI()
@@ -53,5 +53,74 @@ def get_course(course_id: str = Path(..., description="Course ID")):
                 'google-spreadsheet': course_config.get('course', {}).get('google', {}).get('spreadsheet', '')
             }
             return course_info
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get('/courses/{course_id}/groups', response_model=list)
+def get_course_groups(course_id: str = Path(..., description="Course ID")):
+    courses_dir = 'courses'
+    course_file = os.path.join(courses_dir, f'{course_id}.yaml')
+
+    if not os.path.exists(course_file) or not os.path.isfile(course_file):
+        raise HTTPException(status_code=404, detail="Course not found")
+
+    with open(course_file, 'r', encoding='utf-8') as file:
+        try:
+
+            course_config = yaml.safe_load(file)
+            google_spreadsheet_field = course_config.get('course', {}).get('google', {}).get('spreadsheet', '')
+
+            if not google_spreadsheet_field:
+                return []
+
+            groups = google_docs.get_course_groups(google_spreadsheet_field)
+
+            info_sheet_name = course_config.get('course', {}).get('google', {}).get('info-sheet', '')
+
+            if info_sheet_name in groups:
+                groups.remove(info_sheet_name)
+
+            return groups
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+
+@app.get('/courses/{course_id}/groups/{group_id}/labs', response_model=list)
+def get_course_group_labs(
+    course_id: str = Path(..., description="Course ID"),
+    group_id: str = Path(..., description="Group ID")
+):
+    courses_dir = 'courses'
+    course_file = os.path.join(courses_dir, f'{course_id}.yaml')
+
+    if not os.path.exists(course_file) or not os.path.isfile(course_file):
+        raise HTTPException(status_code=404, detail="Course not found")
+
+    with open(course_file, 'r', encoding='utf-8') as file:
+        try:
+            course_config = yaml.safe_load(file)
+            google_spreadsheet = course_config.get('course', {}).get('google', {}).get('spreadsheet', '')
+
+            if not google_spreadsheet:
+                return []
+
+            lab_short_names = []
+
+            # Извлекаем сокращенные названия лабораторных работ из конфигурационного файла
+            lab_config = course_config.get('course', {}).get('labs', {})
+            for lab_key in lab_config:
+                short_name = lab_config[lab_key].get('short-name', '')
+                if short_name:
+                    lab_short_names.append(short_name)
+
+            group_sheets_labs = google_docs.get_course_group_labs(google_spreadsheet, group_id)
+            group_labs = []
+
+            for group in lab_short_names:
+                if group in group_sheets_labs:
+                    group_labs.append(group)
+
+            return group_labs
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
