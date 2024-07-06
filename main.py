c66be1106884823ff93c2f182fe7f24850882f38
ykkssyaa <mr.ender.03@mail.ru>
2024-07-06 19:36:37 +0300

    get_courses

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
+import yaml
 from fastapi import FastAPI
-from gateway import google_docs, github_api
+# from gateway import google_docs, github_api
+import os
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get('/courses/', response_model=list)
+def get_courses():
+    courses_dir = 'courses'
+    courses_info = []
 
+    if not os.path.exists(courses_dir) or not os.path.isdir(courses_dir):
+        return []
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    for filename in os.listdir(courses_dir):
+        file_path = os.path.join(courses_dir, filename)
+        if os.path.isfile(file_path) and filename.endswith('.yaml'):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                try:
+                    course_config = yaml.safe_load(file)
+                    course_info = {
+                        'id': filename,
+                        'name': course_config.get('course', {}).get('name', ''),
+                        'semester': course_config.get('course', {}).get('semester', '')
+                    }
+                    courses_info.append(course_info)
+                except Exception as e:
+                    print(f"Error reading {filename}: {str(e)}")
+
+    return courses_info
